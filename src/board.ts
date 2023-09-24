508899094578edabc39e11b7f89c858356fe1090
jakobgrine <18680782+jakobgrine@users.noreply.github.com>
2023-09-24 13:02:12 +0200

    Format using prettier

--- src/board.ts
+++ src/board.ts
@@ -1,302 +1,305 @@
 interface Coordinate {
-  x: number;
-  y: number;
+    x: number;
+    y: number;
 }
 
 export enum Direction {
-  Left,
-  Up,
-  Right,
-  Down,
+    Left,
+    Up,
+    Right,
+    Down,
 }
 
 export class Board {
-  board: Array<number>;
-  size = 4;
-  gameOver = false;
-  score = 0;
-  highscore = 0;
+    board: Array<number>;
+    size = 4;
+    gameOver = false;
+    score = 0;
+    highscore = 0;
 
-  constructor() {
-    this.score = Number(localStorage.getItem("score"));
-    this.highscore = Number(localStorage.getItem("highscore"));
-    this.gameOver = localStorage.getItem("game_over") === "true";
+    constructor() {
+        this.score = Number(localStorage.getItem("score"));
+        this.highscore = Number(localStorage.getItem("highscore"));
+        this.gameOver = localStorage.getItem("game_over") === "true";
 
-    const savedState = localStorage.getItem("board_state");
-    if (savedState) {
-      this.board = savedState.split(",").map(x => Number(x));
-    } else {
-      this.board = new Array(this.size * this.size).fill(0);
-      this.spawnRandomTile();
-      this.spawnRandomTile();
-    }
-
-    this.apply();
-  }
-
-  reset() {
-    this.score = 0;
-    this.gameOver = false;
-    this.board = new Array(this.size * this.size).fill(0);
-    this.spawnRandomTile();
-    this.spawnRandomTile();
-    this.apply();
-    this.save();
-  }
-
-  save() {
-    localStorage.setItem("score", this.score.toString());
-    localStorage.setItem("highscore", this.highscore.toString());
-    localStorage.setItem("game_over", this.gameOver.toString());
-    localStorage.setItem("board_state", this.board.toString());
-  }
-
-  apply() {
-    const boardElement = document.getElementById("board-foreground");
-    let html = "";
-    for (let row = 0; row < this.size; row++) {
-      for (let col = 0; col < this.size; col++) {
-        const here = { x: col, y: row };
-        if (this.has(here)) {
-          const value = this.get(here);
-          html += `<div class="tile tile-${value}" style="--y: ${row}; --x: ${col};"></div>`;
-        }
-      }
-    }
-    boardElement!.innerHTML = html;
-
-    const scoreElement = document.getElementById("score-value");
-    scoreElement!.innerText = this.score.toString();
-
-    const highscoreElement = document.getElementById("highscore-value");
-    highscoreElement!.innerText = this.highscore.toString();
-
-    if (this.gameOver) {
-      const dialogElement = document.getElementById("restart-dialog") as HTMLDialogElement;
-      dialogElement?.showModal();
-    }
-  }
-
-  action(direction: Direction): boolean {
-    let changed = false;
-    switch (direction) {
-      case Direction.Left:
-        changed = this.left();
-        break;
-      case Direction.Up:
-        changed = this.up();
-        break;
-      case Direction.Right:
-        changed = this.right();
-        break;
-      case Direction.Down:
-        changed = this.down();
-        break;
-    }
-
-    if (changed)
-      this.apply();
-
-    return changed;
-  }
-
-  left(): boolean {
-    const scoreDelta = this.merge();
-    const moved = this.move();
-    this.score += scoreDelta;
-    if (scoreDelta > 0 || moved) {
-      if (this.score > this.highscore) {
-        this.highscore = this.score;
-      }
-
-      this.spawnRandomTile();
-
-      this.save();
-
-      if (this.isGameOver()) {
-        this.gameOver = true;
-      }
-
-      return true;
-    }
-    return false;
-  }
-
-  up(): boolean {
-    this.transpose();
-    const changed = this.left();
-    this.transpose();
-    return changed;
-  }
-
-  right(): boolean {
-    this.transpose();
-    this.reflect();
-    this.transpose();
-    const changed = this.left();
-    this.transpose();
-    this.reflect();
-    this.transpose();
-    return changed;
-  }
-
-  down(): boolean {
-    this.reflect();
-    this.transpose();
-    const changed = this.left();
-    this.transpose();
-    this.reflect();
-    return changed;
-  }
-
-  isGameOver(): boolean {
-    for (let row = 0; row < this.size; row++) {
-      for (let col = 0; col < this.size; col++) {
-        // Check for empty fields
-        const here = { x: col, y: row };
-        if (!this.has(here)) {
-          return false;
+        const savedState = localStorage.getItem("board_state");
+        if (savedState) {
+            this.board = savedState.split(",").map((x) => Number(x));
+        } else {
+            this.board = new Array(this.size * this.size).fill(0);
+            this.spawnRandomTile();
+            this.spawnRandomTile();
         }
 
-        // Check for mergeable tiles
-        const right = { x: col + 1, y: row };
-        if (this.get(here) == this.get(right)) {
-          return false;
-        }
-        const bottom = { x: col, y: row + 1 };
-        if (this.get(here) == this.get(bottom)) {
-          return false;
-        }
-      }
-    }
-    return true;
-  }
-
-  spawnRandomTile() {
-    // Get empty fields
-    const free = new Array<Coordinate>();
-    for (let row = 0; row < this.size; row++) {
-      for (let col = 0; col < this.size; col++) {
-        const here = { x: col, y: row };
-        if (!this.has(here)) {
-          free.push(here);
-        }
-      }
+        this.apply();
     }
 
-    // Pick a random field
-    const index = Math.floor(Math.random() * free.length);
-    // 10% chance of getting a 4, else a 2
-    const value = Math.random() > 0.9 ? 4 : 2;
-    this.set(free[index], value);
-  }
-
-  transpose() {
-    const newBoard = new Array<number>(this.size * this.size).fill(0);
-    for (let row = 0; row < this.size; row++) {
-      for (let col = 0; col < this.size; col++) {
-        // Swap x and y
-        const value = this.get({ x: col, y: row });
-        newBoard[row + col * this.size] = value;
-      }
+    reset() {
+        this.score = 0;
+        this.gameOver = false;
+        this.board = new Array(this.size * this.size).fill(0);
+        this.spawnRandomTile();
+        this.spawnRandomTile();
+        this.apply();
+        this.save();
     }
-    this.board = newBoard;
-  }
 
-  reflect() {
-    const newBoard = new Array<number>(this.size * this.size).fill(0);
-    for (let row = 0; row < this.size; row++) {
-      for (let col = 0; col < this.size; col++) {
-        // Reflect along x-axis
-        const value = this.get({ x: col, y: row });
-        newBoard[col + (this.size - row - 1) * this.size] = value;
-      }
+    save() {
+        localStorage.setItem("score", this.score.toString());
+        localStorage.setItem("highscore", this.highscore.toString());
+        localStorage.setItem("game_over", this.gameOver.toString());
+        localStorage.setItem("board_state", this.board.toString());
     }
-    this.board = newBoard;
-  }
 
-  merge(): number {
-    let scoreDelta = 0;
-    for (let row = 0; row < this.size; row++) {
-      for (let col = 0; col < this.size; col++) {
-        // Look for a non-empty field
-        const left = { x: col, y: row };
-        if (!this.has(left)) {
-          continue;
+    apply() {
+        const boardElement = document.getElementById("board-foreground");
+        let html = "";
+        for (let row = 0; row < this.size; row++) {
+            for (let col = 0; col < this.size; col++) {
+                const here = { x: col, y: row };
+                if (this.has(here)) {
+                    const value = this.get(here);
+                    html += `<div class="tile tile-${value}" style="--y: ${row}; --x: ${col};"></div>`;
+                }
+            }
+        }
+        boardElement!.innerHTML = html;
+
+        const scoreElement = document.getElementById("score-value");
+        scoreElement!.innerText = this.score.toString();
+
+        const highscoreElement = document.getElementById("highscore-value");
+        highscoreElement!.innerText = this.highscore.toString();
+
+        if (this.gameOver) {
+            const dialogElement = document.getElementById(
+                "restart-dialog",
+            ) as HTMLDialogElement;
+            dialogElement?.showModal();
+        }
+    }
+
+    action(direction: Direction): boolean {
+        let changed = false;
+        switch (direction) {
+            case Direction.Left:
+                changed = this.left();
+                break;
+            case Direction.Up:
+                changed = this.up();
+                break;
+            case Direction.Right:
+                changed = this.right();
+                break;
+            case Direction.Down:
+                changed = this.down();
+                break;
         }
 
-        for (let i = col + 1; i < this.size; i++) {
-          // Look for a field with the same value
-          const right = { x: i, y: row };
-          if (!this.has(right)) {
-            continue;
-          }
+        if (changed) this.apply();
 
-          if (this.get(left) == this.get(right)) {
-            // Merge the two values
-            const value = this.get(left)!;
-            this.set(left, value * 2);
-            this.delete(right);
-            scoreDelta += value * 2;
-          }
-          break;
-        }
-      }
+        return changed;
     }
-    return scoreDelta;
-  }
 
-  move(): boolean {
-    let changed = false;
-    for (let row = 0; row < this.size; row++) {
-      for (let col = 0; col < this.size; col++) {
-        // Look for an empty field
-        const left = { x: col, y: row };
-        if (this.has(left)) {
-          continue;
+    left(): boolean {
+        const scoreDelta = this.merge();
+        const moved = this.move();
+        this.score += scoreDelta;
+        if (scoreDelta > 0 || moved) {
+            if (this.score > this.highscore) {
+                this.highscore = this.score;
+            }
+
+            this.spawnRandomTile();
+
+            this.save();
+
+            if (this.isGameOver()) {
+                this.gameOver = true;
+            }
+
+            return true;
+        }
+        return false;
+    }
+
+    up(): boolean {
+        this.transpose();
+        const changed = this.left();
+        this.transpose();
+        return changed;
+    }
+
+    right(): boolean {
+        this.transpose();
+        this.reflect();
+        this.transpose();
+        const changed = this.left();
+        this.transpose();
+        this.reflect();
+        this.transpose();
+        return changed;
+    }
+
+    down(): boolean {
+        this.reflect();
+        this.transpose();
+        const changed = this.left();
+        this.transpose();
+        this.reflect();
+        return changed;
+    }
+
+    isGameOver(): boolean {
+        for (let row = 0; row < this.size; row++) {
+            for (let col = 0; col < this.size; col++) {
+                // Check for empty fields
+                const here = { x: col, y: row };
+                if (!this.has(here)) {
+                    return false;
+                }
+
+                // Check for mergeable tiles
+                const right = { x: col + 1, y: row };
+                if (this.get(here) == this.get(right)) {
+                    return false;
+                }
+                const bottom = { x: col, y: row + 1 };
+                if (this.get(here) == this.get(bottom)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    spawnRandomTile() {
+        // Get empty fields
+        const free = new Array<Coordinate>();
+        for (let row = 0; row < this.size; row++) {
+            for (let col = 0; col < this.size; col++) {
+                const here = { x: col, y: row };
+                if (!this.has(here)) {
+                    free.push(here);
+                }
+            }
         }
 
-        for (let i = col + 1; i < this.size; i++) {
-          // Look for a non-empty field
-          const right = { x: i, y: row };
-          if (!this.has(right)) {
-            continue;
-          }
+        // Pick a random field
+        const index = Math.floor(Math.random() * free.length);
+        // 10% chance of getting a 4, else a 2
+        const value = Math.random() > 0.9 ? 4 : 2;
+        this.set(free[index], value);
+    }
 
-          // Move the non-empty field to the left
-          this.set(left, this.get(right)!);
-          this.delete(right);
-          changed = true;
-          break;
+    transpose() {
+        const newBoard = new Array<number>(this.size * this.size).fill(0);
+        for (let row = 0; row < this.size; row++) {
+            for (let col = 0; col < this.size; col++) {
+                // Swap x and y
+                const value = this.get({ x: col, y: row });
+                newBoard[row + col * this.size] = value;
+            }
         }
-      }
+        this.board = newBoard;
     }
-    return changed;
-  }
 
-  get(coordinate: Coordinate) {
-    return this.board[coordinate.x + coordinate.y * this.size];
-  }
-
-  set(coordinate: Coordinate, value: number) {
-    this.board[coordinate.x + coordinate.y * this.size] = value;
-  }
-
-  has(coordinate: Coordinate) {
-    return this.get(coordinate) > 0;
-  }
-
-  delete(coordinate: Coordinate) {
-    this.set(coordinate, 0);
-  }
-
-  print() {
-    let str = "";
-    for (let row = 0; row < this.size; row++) {
-      str += this.board.slice(row * this.size, (row + 1) * this.size).join(" ");
-      str += "\n";
+    reflect() {
+        const newBoard = new Array<number>(this.size * this.size).fill(0);
+        for (let row = 0; row < this.size; row++) {
+            for (let col = 0; col < this.size; col++) {
+                // Reflect along x-axis
+                const value = this.get({ x: col, y: row });
+                newBoard[col + (this.size - row - 1) * this.size] = value;
+            }
+        }
+        this.board = newBoard;
     }
-    console.log(str);
-    console.log(this.board);
-  }
+
+    merge(): number {
+        let scoreDelta = 0;
+        for (let row = 0; row < this.size; row++) {
+            for (let col = 0; col < this.size; col++) {
+                // Look for a non-empty field
+                const left = { x: col, y: row };
+                if (!this.has(left)) {
+                    continue;
+                }
+
+                for (let i = col + 1; i < this.size; i++) {
+                    // Look for a field with the same value
+                    const right = { x: i, y: row };
+                    if (!this.has(right)) {
+                        continue;
+                    }
+
+                    if (this.get(left) == this.get(right)) {
+                        // Merge the two values
+                        const value = this.get(left)!;
+                        this.set(left, value * 2);
+                        this.delete(right);
+                        scoreDelta += value * 2;
+                    }
+                    break;
+                }
+            }
+        }
+        return scoreDelta;
+    }
+
+    move(): boolean {
+        let changed = false;
+        for (let row = 0; row < this.size; row++) {
+            for (let col = 0; col < this.size; col++) {
+                // Look for an empty field
+                const left = { x: col, y: row };
+                if (this.has(left)) {
+                    continue;
+                }
+
+                for (let i = col + 1; i < this.size; i++) {
+                    // Look for a non-empty field
+                    const right = { x: i, y: row };
+                    if (!this.has(right)) {
+                        continue;
+                    }
+
+                    // Move the non-empty field to the left
+                    this.set(left, this.get(right)!);
+                    this.delete(right);
+                    changed = true;
+                    break;
+                }
+            }
+        }
+        return changed;
+    }
+
+    get(coordinate: Coordinate) {
+        return this.board[coordinate.x + coordinate.y * this.size];
+    }
+
+    set(coordinate: Coordinate, value: number) {
+        this.board[coordinate.x + coordinate.y * this.size] = value;
+    }
+
+    has(coordinate: Coordinate) {
+        return this.get(coordinate) > 0;
+    }
+
+    delete(coordinate: Coordinate) {
+        this.set(coordinate, 0);
+    }
+
+    print() {
+        let str = "";
+        for (let row = 0; row < this.size; row++) {
+            str += this.board
+                .slice(row * this.size, (row + 1) * this.size)
+                .join(" ");
+            str += "\n";
+        }
+        console.log(str);
+        console.log(this.board);
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,26 @@
 import { Board, Direction } from "./board";
 
 function main() {
-  const board = new Board();
+    const board = new Board();
 
-  window.addEventListener("keydown", (event) => {
-    switch (event.code) {
-      case "ArrowLeft":
-      case "ArrowUp":
-      case "ArrowRight":
-      case "ArrowDown":
-        const direction = Direction[event.code.replace("Arrow", "")];
-        board.action(direction);
-        break;
-    }
-  });
+    window.addEventListener("keydown", (event) => {
+        switch (event.code) {
+            case "ArrowLeft":
+            case "ArrowUp":
+            case "ArrowRight":
+            case "ArrowDown":
+                const direction = Direction[event.code.replace("Arrow", "")];
+                board.action(direction);
+                break;
+        }
+    });
 
-  document.getElementById("reset-button")!.addEventListener("click", () => {
-    board.reset();
-  });
-  document.getElementById("restart-button")!.addEventListener("click", () => {
-    board.reset();
-  });
+    document.getElementById("reset-button")!.addEventListener("click", () => {
+        board.reset();
+    });
+    document.getElementById("restart-button")!.addEventListener("click", () => {
+        board.reset();
+    });
 }
 
 window.addEventListener("load", main);
